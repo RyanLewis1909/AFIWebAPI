d2c58c8614e3104d50d04148675c3bfb1cf8c70a
Ryan Lewis <ryan@mmxsystems.co.za>
2021-06-24 15:44:49 +0200

    Added some SQLite tests

---
+++ AFIWebAPI.Tests/CustomersContextTests.cs
@@ -0,0 +1,83 @@
+﻿using AFIWebAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace AFIWebAPI.Tests
+{
+    public class CustomersContextTests : TestWithSqlite
+    {
+        readonly Customer newItem = new Customer()
+        {
+            ID = Guid.NewGuid(),
+            FirstName = "TestName",
+            Surname = "TestSurname",
+            RefNo = "TT-123456",
+            DateOfBirth = DateTime.Parse("1994/09/19"),
+            Email = "test@example.com"
+        };
+
+        [Fact]
+        public async Task DatabaseIsAvailableAndCanBeConnectedTo()
+        {
+            Assert.True(await DbContext.Database.CanConnectAsync());
+        }
+
+        [Fact]
+        public void TableShouldGetCreated()
+        {
+            Assert.False(DbContext.Customers.Any());
+        }
+
+        [Fact]
+        public void FirstNameCannotBeNull()
+        {
+            newItem.FirstName = null;
+            DbContext.Customers.Add(newItem);
+
+            Assert.Throws<DbUpdateException>(() => DbContext.SaveChanges());
+        }
+
+        [Fact]
+        public void AddedItemShouldGetGenerated()
+        {
+            DbContext.Customers.Add(newItem);
+            DbContext.SaveChanges();
+
+            Assert.NotEqual(Guid.Empty, newItem.ID);
+        }
+
+        [Fact]
+        public void AddedItemShouldGetPersisted()
+        {
+            DbContext.Customers.Add(newItem);
+            DbContext.SaveChanges();
+
+            Assert.Equal(newItem, DbContext.Customers.Find(newItem.ID));
+            Assert.Equal(1, DbContext.Customers.Count());
+        }
+
+        [Fact]
+        public void GetAddedItemByIdAndMatchEmail()
+        {
+            DbContext.Customers.Add(newItem);
+            DbContext.SaveChanges();
+
+            Assert.Equal("test@example.com", DbContext.Customers.Find(newItem.ID).Email);
+        }
+
+        [Fact]
+        public void GetAddedItemShouldGetUpdated()
+        {
+            newItem.FirstName = "Updated";
+            DbContext.Customers.Add(newItem);
+            DbContext.SaveChanges();
+
+            Assert.Equal("Updated", DbContext.Customers.Find(newItem.ID).FirstName);
+        }
+    }
+}
---
+++ AFIWebAPI.Tests/TestWithSqlite.cs
@@ -0,0 +1,35 @@
+﻿using AFIWebAPI.Models;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AFIWebAPI.Tests
+{
+    public abstract class TestWithSqlite : IDisposable
+    {
+        private const string InMemoryConnectionString = "DataSource=:memory:";
+        private readonly SqliteConnection _connection;
+
+        protected readonly CustomersContext DbContext;
+
+        protected TestWithSqlite()
+        {
+            _connection = new SqliteConnection(InMemoryConnectionString);
+            _connection.Open();
+            var options = new DbContextOptionsBuilder<CustomersContext>()
+                    .UseSqlite(_connection)
+                    .Options;
+            DbContext = new CustomersContext(options);
+            DbContext.Database.EnsureCreated();
+        }
+
+        public void Dispose()
+        {
+            _connection.Close();
+        }
+    }
+}
